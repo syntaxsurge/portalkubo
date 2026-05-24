e38b3846baf29f3b3cf87a635c260b1e2bb2ec71
Syntax Surge <syntaxsurge@gmail.com>
2026-05-24 21:40:32 +0800

    Remove top loader from wallet session refresh

--- src/components/providers/wallet-session-bridge.tsx
+++ src/components/providers/wallet-session-bridge.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import * as React from 'react'
-
-import { useRouter } from 'nextjs-toploader/app'
 
 import {
   WALLET_ADDRESS_COOKIE,
